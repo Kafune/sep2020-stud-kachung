c0bb1d3ff768bd6b1ad08723a5768475056430e3
Kafune <kachung97@hotmail.com>
2020-09-12 02:03:49 +0200

    begin express

--- swd/unit03/session3-1/myapp/app.js
+++ swd/unit03/session3-1/myapp/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let nRequests = 0;
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/', (req, res) => res.send('Hello World! '))
+
+nRequests++;
+console.log(nRequests);
+
+app.listen(3000, () => console.log('Example app listening on port 3000! '))
+
