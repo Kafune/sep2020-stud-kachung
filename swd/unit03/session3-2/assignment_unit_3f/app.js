e32157e1a53884e746595d917dc17c698abb9efe
Kafune <kachung97@hotmail.com>
2020-09-15 01:11:22 +0200

    assignment 3f tot aan actions

--- swd/unit03/session3-2/assignment_unit_3f/app.js
+++ swd/unit03/session3-2/assignment_unit_3f/app.js
@@ -8,46 +8,46 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const games = require('./routes/games');
+
 const gameFilesFolderName = 'game_files';
 
-app.use(bodyParser.json());
-
 const gameFileReader = async (req, res, next) => {
-  //TODO D1, D2
-  try {
-    req.fileName = path.join(gameFilesFolderName, `${req.params.player}.json`);
-    req.fileContent = await promiseWrappers.readFileP(req.fileName);
-    next();
-  } catch(error) {
-    if(error) {
-      res.status(404);
-      res.json(error);
-    } else {
-      error = new Error('Player does not exist!');
-      next(error);
+    //TODO D1, D2
+    try {
+        req.fileName = path.join(gameFilesFolderName, `${req.params.player}.json`);
+        req.fileContent = await promiseWrappers.readFileP(req.fileName);
+        next();
+    } catch (error) {
+        if (error) {
+            res.status(404);
+            res.json(error);
+        } else {
+            error = new Error('Player does not exist!');
+            next(error);
+        }
     }
-  }
 }
 
-const gameStateReader = async(req, res, next) => {
-  //TODO D3
-  try {
-    req.gameState = JSON.parse(req.fileContent);
-    req.game = new Game(req.gameState);
-    next();
-  } catch(error) {
-    error = new Error('Game does not exist!');
-    next(error);
-  }
+const gameStateReader = async (req, res, next) => {
+    //TODO D3
+    try {
+        req.gameState = JSON.parse(req.fileContent);
+        req.game = new Game(req.gameState);
+        next();
+    } catch (error) {
+        error = new Error('Game does not exist!');
+        next(error);
+    }
 }
 
 app.use('/action/:player/', gameFileReader);
 app.use('/action/:player/', gameStateReader);
 
-app.get('/listPlayerFiles', async(req, res) => {
-  const directory = await promiseWrappers.readdirP(gameFilesFolderName);
-  res.json(directory);
-})
+app.use(bodyParser.json());
+app.use('/games', games);
+
+
 
 app.get('/action/:player/where', async (req, res) => {
     const locationInformation = await req.game.getLocationInformation();
@@ -71,30 +71,7 @@ app.post('/action/:player/arise', async (req, res) => {
   res.send(playerNewStart);
 });
 
-app.post('/createPlayerFile', async(req, res) => {
-    try {
-      const fileLocation = gameFilesFolderName + `/${req.body.name}.json`;
-      const newFile = await promiseWrappers.createEmptyFileP(fileLocation);
-      await promiseWrappers.writeFileP(fileLocation, JSON.stringify(req.body));
-      res.send(req.body)
-    } catch(error) {
-      error = "Er is iets fout gegaan bij het aanmaken van de speler.";
-      res.json(error);
-  }
-});
-
-app.delete('/deletePlayerFile/:player', async(req, res) => {
-    const message = {
-      "result" : `Game ${req.params.player}.json removed`
-    }
-    try {
-      await promiseWrappers.unlinkFileP(gameFilesFolderName + `/${req.params.player}.json`);
-      res.json(message);
-    } catch(error) {
-        res.json(error);
-    }
-})
-
 const server = app.listen(3000, () => {
     console.log(`game server started on port ${server.address().port}`);
 });
+
