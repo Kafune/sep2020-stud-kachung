6a15e6d30feb3f48bea760431974a605172f0f0d
Kafune <kachung97@hotmail.com>
2020-09-24 12:09:25 +0200

    Update Preferences.js

--- cwd/unit04/session 4.1/workshop/RrHN-client/src/components/Preferences.js
+++ cwd/unit04/session 4.1/workshop/RrHN-client/src/components/Preferences.js
@@ -1,46 +1,48 @@
 import React from 'react';
 
-export default function Preferences(props) {
-    const checkInput = (e) => {
-        // e.preventDefault;
-        props.preferenceHandler(props.preferenceHandler);
-        console.log(props.preferenceHandler)
-    }
+// export default function Preferences(props) {
+//     const checkInput = (e) => {
+//         // e.preventDefault;
+//         props.preferenceHandler(props.preferenceHandler);
+//         console.log(props.preferenceHandler)
+//     }
 
-    return <div id="ItemPanel" class="preferences">
-    <div class="PreferencesDialog">
-        <header>
-            <div class="Logo">
-                <div class="colored">RrHN</div>
-                <div class="title">Settings</div>
-            </div>
-        </header>
-        <label for="listSizeField">
-            Show <input type="number" name="itemSize" id="listSizeField" value={props.options.itemSize} onChange={checkInput}/> items in the list.
-        </label>
-        <label for="colorField">
-            color:
-             <select id="colorField" name="color" onChange={checkInput} value={props.options.color}>
-                <option value="orange">orange</option>
-                <option value="green">green</option>
-                <option value="brown">brown</option>
-            </select>
-        </label>
-        <div class="dialogButtons"><button>OK</button><button>Cancel</button></div>
-    </div>
-</div>
-}
+//     return <div id="ItemPanel" class="preferences">
+//     <div class="PreferencesDialog">
+//         <header>
+//             <div class="Logo">
+//                 <div class="colored">RrHN</div>
+//                 <div class="title">Settings</div>
+//             </div>
+//         </header>
+//         <label for="listSizeField">
+//             Show <input type="number" name="itemSize" id="listSizeField" value={props.options.itemSize} onChange={checkInput}/> items in the list.
+//         </label>
+//         <label for="colorField">
+//             color:
+//              <select id="colorField" name="color" onChange={checkInput} value={props.options.color}>
+//                 <option value="orange">orange</option>
+//                 <option value="green">green</option>
+//                 <option value="brown">brown</option>
+//             </select>
+//         </label>
+//         <div class="dialogButtons"><button>OK</button><button>Cancel</button></div>
+//     </div>
+// </div>
+// }
 
 export default class Preferences extends React.Component {
-    constructor(props) {
-        super.props;
-        this.state = {
-            ... this.props.preferences,
-        };
+
+    state = {
+        ... this.props.preferences,
+    };
+
+    handleColorChange = (e) => {
+        this.setState({color: e.target.value})
     }
 
-    handleColorChange(e) {
-        this.setState({color: e.target.value})
+    handleItemSize = (e) => {
+        this.setState({itemSize: e.target.value});
     }
     
     saveThePrefs = () => this.props.savePrefs({color: this.state.color, itemSize: this.state.itemSize})
@@ -56,7 +58,7 @@ export default class Preferences extends React.Component {
                 </div>
             </header>
             <label for="listSizeField">
-                Show <input type="number" name="itemSize" id="listSizeField" value={props.options.itemSize} onChange={checkInput}/> items in the list.
+                Show <input type="number" name="itemSize" id="listSizeField" value={this.props.options.itemSize} onChange={this.handleItemSize}/> items in the list.
             </label>
             <label for="colorField">
                 color:
