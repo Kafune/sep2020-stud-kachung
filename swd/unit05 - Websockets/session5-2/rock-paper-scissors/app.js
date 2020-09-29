b8cfeb650fa956947e9c81b97ead32886ed69fda
Kafune <kachung97@hotmail.com>
2020-09-29 14:15:01 +0200

    Update app.js

--- swd/unit05 - Websockets/session5-2/rock-paper-scissors/app.js
+++ swd/unit05 - Websockets/session5-2/rock-paper-scissors/app.js
@@ -1,24 +1,20 @@
-var express = require('express');
-var ws = require('ws');
-var http = require('http');
-var path = require('path');
+var express = require("express");
+var ws = require("ws");
+var http = require("http");
+var path = require("path");
 
 var expressApp = express();
 var httpServer = http.createServer();
 var webSocketServer = new ws.Server({
-   server: httpServer,
-   verifyServer: hasTwoConnections
+  server: httpServer
 });
 
-function hasTwoConnections() {
-   return webSocketServer.clients.size <= 2;
-}
 
-// Code to setup the Express app (middleware, routes) can go here.
-expressApp.use(express.static(path.join(__dirname, 'client-side')));
+// code to setup the Express app (middleware, routes) can go here; eg:
+expressApp.use(express.static(path.join(__dirname, "client-side")));
 
 const rules = {
-   rock: {
+   rock:{
       scissors: true,
       paper: false
    },
@@ -26,72 +22,87 @@ const rules = {
       rock: false,
       paper: true
    },
-   paper: {
+   paper:{
       rock: true,
       scissors: false
    }
 }
 
+
 const prefabMessage = {
-   CHOICE_ACCEPTED : sendChoiceAcceptedMessage,
-   CHOICE_NOT_ACCEPTED : () => {},
-   OPPONENT_CHOICE : () => {},
-   WIN: () => {},
-   LOSER: () => {}
-}
-function choiceAcceptedMessage(message) {
-
-}
-// max 2 spelers, juiste messagetypes returnen, als een game gespeeld is, bijhouden wat de stand is
-function winMessage() {
-   return JSON.Stringify({messageType: "WIN", ownScore: ""})
+   CHOICE_ACCEPTED : choiceAcceptedMessage,
+   CHOICE_NOT_ACCEPTED : () =>{},
+   OPPONENT_CHOICE : () =>{},
+   WIN: winMessage,
+   LOSE: ()=>{},
+   TIE: tieMessage,
+   OPPONENT_LEFT: ()=>{}
 }
 
+//TODO: Add some nessecary properties
+function choiceAcceptedMessage(){
+   return JSON.stringify({messageType: "CHOICE ACCEPTED" })
+}
 
-function playGame(player1Socket, player2Socket) {
-   if (player1Socket.choice === player2Socket.choice) {
-      return {isTie = true, player1: player1Socket, player2: player2Socket}
-   } else if (rules[player1Socket.choice][player2Socket.choice]) {
-      return {isTie = false, winner: player1Socket, loser: player2Socket}
-   } else {
-      return {isTie = false, winner: player2Socket, loser: player1Socket}
+//TODO: Add some nessecary properties
+function tieMessage(){
+   return JSON.stringify({messageType: "TIE" })
+}
+
+//TODO: Add some nessecary properties
+function winMessage(){
+   return JSON.stringify({messageType: "WIN" })
+}
+
+
+function playGame(player1Socket, player2Socket){
+   console.log(player1Socket.choice)
+   if(player1Socket.choice.toLowerCase() === player2Socket.choice.toLowerCase()){
+      return {isTie : true, player1: player1Socket, player2: player2Socket}
+   }else if(rules[player1Socket.choice.toLowerCase()][player2Socket.choice.toLowerCase()]){
+      return {isTie: false, winner: player1Socket, loser: player2Socket}
+   }else{
+      return {isTie: false, winner: player2Socket, loser: player1Socket}
    }
 }
 
-// code to setup event listeners for WebSocket communication can go here
 webSocketServer.on('connection', function connection(websocket) {
-   console.log("Connection CREATED");
+   console.log("CONNECTION CREATED");
 
    websocket.on('message', function incoming(message) {
-      const messageObject = JSON.parse(message);
-      websocket.choice = messageObject.choice;
+      
+      
+      const messageObject = JSON.parse(message)
+      websocket.choice = messageObject.choice
+     
+      //Check whether or not both servers have chosen something
+      //beware! webSocketServer.clients is a set, not an array. So use size and destructuring.
+      const [client1, client2] = webSocketServer.clients
+      if(webSocketServer.clients.size == 2 && client1.choice != undefined && client2.choice != undefined){
+         
+         let result = playGame(client1, client2)
 
-      console.log(webSocketServer.clients.size);
-
-      if (webSocketServer.clients.size == 2) {
-         // client1.partner = client2;
-         // client2.partner = client1;
-         // client1.partner.forEach();
-
-         let result = playGame(...webSocketServer.clients())
-
-         if(result.isTie) {
-            //tie
-            console.log("tie!")
-            result.player1.send(sendMessage.TIE(result))
-            result.player2.send(sendMessage.tie(result))
-         } else {
-            result.winner.send(prefabMessage.WIN(result))
-            result.loser.send("loser message")
+         if(result.isTie){
+            console.log("tie")
+            result.player1.send(prefabMessage.TIE())
+            result.player2.send(prefabMessage.TIE())
+         }else{
+            console.log("winner")
+            result.winner.send(prefabMessage.WIN())
+            result.loser.send(prefabMessage.LOSE())
          }
+        // webSocketServer.clients.forEach((server)=>{server.choice = undefined})
       }
-      // const result = playGame(eerste, tweede);
-
+      
+   
    });
+
+
 });
 
-httpServer.on('request', expressApp);
-httpServer.listen(3000,
-   function () {
-      console.log("The Server is lisening on port 3000.")
-   });
+
+// connect the Express App to all incoming requests on the HTTP server
+httpServer.on("request", expressApp);
+httpServer.listen(3000, function() {
+  console.log("The Server is listening on port 3000.");
+});
