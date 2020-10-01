1204012701a8ee259de49c2c69be7da30d5f8fca
Kafune <kachung97@hotmail.com>
2020-10-01 22:42:03 +0200

    cwd assignment 5.4

--- cwd/unit05/session5.2/RrHN-client/src/components/App.jsx
+++ cwd/unit05/session5.2/RrHN-client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import ItemList from './ItemList';
 import Preferences from './Preferences';
--- cwd/unit05/session5.2/workshop/src/components/App.js
+++ cwd/unit05/session5.2/workshop/src/components/App.js
@@ -8,50 +8,38 @@ import eevee from '../eevee.json'
 import { uniq } from 'lodash'
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon"
+ 
 
-  // function searchPokemon (query) {
-  //   this.setState({query: query})
-  //   fetch(`${API_URL}/${query}`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     this.setState({
-  //       activePokemon: data,
-  //       history: uniq(this.state.history.concat(query))
-  //     })
-  //   })
-  // }
+  function App (props) {
 
-function App (props) {
 
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     query: '',
-  //     history: ['charizard', 'vaporeon', 'bulbasaur'],
-  //     activePokemon: eevee
-  //   }
-  // }
-
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [history, setHistory] = useState(['charizard', 'vaporeon', 'bulbasaur'])
   const [activePokemon, setActivePokemon] = useState(eevee);
 
+  useEffect(() => {
+    fetch(`${API_URL}/${query}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setHistory(activePokemon.name)
+      setActivePokemon(data);
+      // setActivePokemon(eevee)
+    })
+  })
 
-  // render () {
-    console.log(activePokemon)
     return <div className="App">
       <header className="App-header">
         <p>Find your own Pokemon now!</p>
         <div className="search">
           <FindPokemon value={query} onChange={(e) => setQuery(e.target.value)} />
           <SearchHistory
-            onClick={() => {setHistory(activePokemon)}}
+            onClick={(historical) => {setActivePokemon(historical)}}
             history={history}/>
         </div>
       </header>
       <Pokemon pokemon={activePokemon} />
     </div>
-  // }
 }
 
 export default App;
